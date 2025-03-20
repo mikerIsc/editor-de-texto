32b36c568e84b3ad78b4b22d2a02fa7e4cadff6a
MutableName96 <antoniolopezmartinez45@gmail.com>
2025-03-20 09:34:15 -0600

    js

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
-<<<<<<< HEAD
 
-function buscarTexto() {
-    let texto = document.getElementById("editor").innerHTML;
-    let palabra = document.getElementById("buscar").value;
-    let regex = new RegExp(`(${palabra})`, "gi");
-    let nuevoTexto = texto.replace(regex, '<mark>$1</mark>');
-    document.getElementById("editor").innerHTML = nuevoTexto;
-}
-
-function ponerNegrita() {
-    document.execCommand('bold');
-}
-
-function convertirMayusculas() {
-    let editor = document.getElementById("editor");
-    editor.innerHTML = editor.innerText.toUpperCase();
-}
-
-function convertirMinusculas() {
-    let editor = document.getElementById("editor");
-    editor.innerHTML = editor.innerText.toLowerCase();
-}
-
-function reemplazarTexto() {
-    let palabra = prompt("Palabra a reemplazar:");
-    let nuevaPalabra = prompt("Nueva palabra:");
-    let editor = document.getElementById("editor");
-    let regex = new RegExp(palabra, "gi");
-    editor.innerHTML = editor.innerText.replace(regex, nuevaPalabra);
-}
-=======
-function buscarTexto() {
+        function buscarTexto() {
             let texto = document.getElementById("editor").innerHTML;
             let palabra = document.getElementById("buscar").value;
             let regex = new RegExp(`(${palabra})`, "gi");
@@ -59,4 +28,3 @@ function buscarTexto() {
             let regex = new RegExp(palabra, "gi");
             editor.innerHTML = editor.innerText.replace(regex, nuevaPalabra);
         }
->>>>>>> f35b8ffb99597ac0f5e8425c14d183b34ddba2af
